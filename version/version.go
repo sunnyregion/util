e085a58a2aa64e0f59d33ef3867127a1c20ed486
sunny <jinheking@sina.com>
2018-09-08 17:26:29 +0800

    :art:

--- version/version.go
+++ version/version.go
@@ -7,10 +7,10 @@ import (
 )
 
 var (
-	BuildVersion string
-	BuildTime    string
-	BuildName    string
-	CommitID     string
+	VERSION    string
+	BUILD_TIME string
+	BUILD_NAME string
+	GO_VERSION string
 )
 
 func init() {
@@ -18,11 +18,11 @@ func init() {
 	flag.BoolVar(&showVer, "v", false, "show build version")
 	flag.Parse()
 	if showVer {
-		// Printf( "build name:\t%s\nbuild ver:\t%s\nbuild time:\t%s\nCommitID:%s\n", BuildName, BuildVersion, BuildTime, CommitID )
-		fmt.Printf("build name:\t%s\n", BuildName)
-		fmt.Printf("build ver:\t%s\n", BuildVersion)
-		fmt.Printf("build time:\t%s\n", BuildTime)
-		fmt.Printf("Commit ID:\t%s\n", CommitID)
+		fmt.Printf("Build Name:\t%s\n", BUILD_NAME)
+		fmt.Printf("Build Version:\t%s\n", VERSION)
+		fmt.Printf("Build Time:\t%s\n", BUILD_TIME)
+		fmt.Printf("Git ID:\t%s\n", GO_VERSION)
+		fmt.Println(`-----🎉🎉🎉👍💁👌⚽🎍😍🎉🎉🎉------`)
 		os.Exit(0)
 	}
 }
