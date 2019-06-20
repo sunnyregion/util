37ba1ced8cc0f82d6e34896e3171a2e9d938c6b1
sunny <qi.cao@cloudminds.com>
2019-06-20 17:03:04 +0800

    :art: struct

--- struct.go
+++ struct.go
@@ -2,7 +2,7 @@ package util
 
 // SunnyMsgStruct 统一返回的信息结构体
 type SunnyMsgStruct struct {
-	Msg    string
-	Status bool
-	Data   interface{}
+	Msg    string      `json:msg`
+	Status bool        `json:status`
+	Data   interface{} `json:data`
 }
