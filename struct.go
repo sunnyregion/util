fc8eef0e7493350fe24c6f73eb8ad61f2bb154ab
Sunny <jinheking@sina.com>
2019-06-22 08:59:10 +0800

    Update struct.go

--- struct.go
+++ struct.go
@@ -2,7 +2,7 @@ package util
 
 // SunnyMsgStruct 统一返回的信息结构体
 type SunnyMsgStruct struct {
-	Msg    string      `json:“msg”`
-	Status bool        `json:”status“`
-	Data   interface{} `json:“data”`
+	Msg    string      `json:"msg"`
+	Status bool        `json:"status"`
+	Data   interface{} `json:"data"`
 }
